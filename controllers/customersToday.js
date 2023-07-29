9fa1ee74eb2da7bf0fad190054467dae54f33d87
Bolatito Adeoye <titoadeoye00@gmail.com>
2023-07-29 18:20:35 +0100

    fix - bug caused by not validating response from db
    Validated that response object type contains customers before running block of code

--- controllers/customersToday.js
+++ controllers/customersToday.js
@@ -1,61 +1,81 @@
 const jwtDecode = require("jwt-decode");
-const jwt = require('jsonwebtoken');
-
+const jwt = require("jsonwebtoken");
 
 const customersToday = async (req, res, client, dbName, ObjectId) => {
-    const reqId = req.params.tailorId;
-    const bearer = req?.headers?.authorization;
+  const reqId = req.params.tailorId;
+  const bearer = req?.headers?.authorization;
 
-    if (bearer) {
-        const [, token] = bearer ? bearer.split(" ") : res.status(401).json({ message: "Unauthorized. Access is denied due to invalid credentials." })
-            ;
-        const payload = token ? jwt.verify(token, process.env.SECRET_KEY) : null;
-        if (!!payload) {
-            const decoded = jwtDecode(token);
-            const tailor = reqId === decoded.sub ? decoded.sub : null;
+  if (bearer) {
+    const [, token] = bearer
+      ? bearer.split(" ")
+      : res
+          .status(401)
+          .json({
+            message:
+              "Unauthorized. Access is denied due to invalid credentials."
+          });
+    const payload = token ? jwt.verify(token, process.env.SECRET_KEY) : null;
+    if (!!payload) {
+      const decoded = jwtDecode(token);
+      const tailor = reqId === decoded.sub ? decoded.sub : null;
 
-            try {
-                await client.connect();
-                const db = client.db(dbName);
-                const col = db.collection("customers");
+      try {
+        await client.connect();
+        const db = client.db(dbName);
+        const col = db.collection("customers");
 
-                tailor ?
-                    col?.findOne({ id: ObjectId(tailor) })
-                        .then(async (response) => {
-                            if (response.customers) {
-                                const data = response.customers;
-                               
-                                var nd = data.filter(customer => {
-                                    const today = new Date();
+        tailor
+          ? col
+              ?.findOne({ id: ObjectId(tailor) })
+              .then(async (response) => {
+                if (response) {
+                  if (
+                    Object.keys(response).includes("customers") &&
+                    response.customers
+                  ) {
+                    const data = response.customers;
 
-                                    return today.toDateString() === customer.dateCreated.toDateString();
-                                });
-                               
-                                res.status(200).json({ data: nd.length });
-                            } else if ((!response.customers)) {
-                               
-                                res.status(200).json({ data: 0 });
-                            }
-                        })
-                        .catch(err => {
-                            res.status(401).json({ message: "Unauthorized. Access is denied due to invalid credentials." })
-                        })
-                    : res.status(400).json({ message: "User does not exist" })
+                    var nd = data.filter((customer) => {
+                      const today = new Date();
 
-            } catch (err) {
-                res.status(500).json({ message: "Something went wrong" });
-            }
+                      return (
+                        today.toDateString() ===
+                        customer.dateCreated.toDateString()
+                      );
+                    });
 
-
-        } else {
-            res.status(401).json({ message: "Uauthorized. Access is denied due to invalid credentials." })
-
-        }
+                    res.status(200).json({ data: nd.length });
+                  } else if (!response.customers) {
+                    res.status(200).json({ data: 0 });
+                  }
+                } else {
+                    res.status(200).json({ data: 0 })
+                }
+              })
+              .catch((err) => {
+                console.log("today", err);
+                res.status(401).json({ message: err });
+              })
+          : res.status(400).json({ message: "User does not exist" });
+      } catch (err) {
+        res.status(500).json({ message: "Something went wrong" });
+      }
     } else {
-        res.status(401).json({ message: "Unauthorized. Access is denied due to invalid credentials." })
-
+      res
+        .status(401)
+        .json({
+          message:
+            "payload, Uauthorized. Access is denied due to invalid credentials."
+        });
     }
+  } else {
+    res
+      .status(401)
+      .json({
+        message:
+          "bearer, Unauthorized. Access is denied due to invalid credentials."
+      });
+  }
+};
 
-}
-
-module.exports = { customersToday }
+module.exports = { customersToday };
